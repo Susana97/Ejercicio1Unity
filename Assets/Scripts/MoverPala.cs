213351d279eae98a9c4c1fb7f45326ab05409ee7
Susana Santos Moreno <susiylola@gmail.com>
2018-12-17 11:25:08 +0100

    Commit inicial
    Con los objetos creados.

---
+++ Assets/Scripts/MoverPala.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPala : MonoBehaviour {
+
+    public float velocidad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Vertical") == 1){//hacia arriba
+            float ejeY = -1;
+            ejeY = ejeY + Input.GetAxisRaw("Vertical") * velocidad * Time.deltaTime;
+        }
+        if(Input.GetAxisRaw("Vertical") == -1){//hacia abajo
+            float ejey = Input.GetAxisRaw("Vertical") * velocidad * Time.deltaTime;
+        }
+	}
+}
